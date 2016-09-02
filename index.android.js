8b9100971e9b1bcb33687377ab80784b77aecac6
Fabian <bongosart@gmail.com>
2016-09-02 15:11:40 +0200

    Init collabsheet-camera-app
    This is the init commit for the collabsheet camera app.

---
+++ index.android.js
@@ -0,0 +1,70 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
+
+class BadInstagramCloneApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+          playSoundOnCapture={Camera.constants.Aspect.playSoundOnCapture=false}>
+
+          <TouchableHighlight onPress={this.takePicture.bind(this)}>
+            <Text style={styles.button}>Tap</Text>
+          </TouchableHighlight>
+        </Camera>
+      </View>
+    );
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'stretch',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 1,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  },
+  button: {
+    flex: 0,
+    backgroundColor: '#EAF3FC',
+    borderRadius: 5,
+    width: 50,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+AppRegistry.registerComponent('CameraApp', () => BadInstagramCloneApp);
